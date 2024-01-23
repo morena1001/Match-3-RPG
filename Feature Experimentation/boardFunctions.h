e2c4f5f7358d0cb758d02e2c18b7450f27df71f3
josue-flores03 <josue.flores03@utrgv.edu>
2024-01-22 22:41:33 -0600

    new features added

--- Feature Experimentation/boardFunctions.h
+++ Feature Experimentation/boardFunctions.h
@@ -14,6 +14,8 @@ void printBoard(piece board[5][7]);
 void matchChecking(piece board[5][7]);
 void pieceShuffle(piece board[5][7]);
 void getMoves(piece board[5][7]);
+void checkMatchAfterMove(piece board[5][7], int x, int y);
+void removeMatched(piece board[5][7]);
 
 
 void generateNewPieces(piece board[5][7]) {
@@ -54,26 +56,52 @@ void matchChecking(piece board[5][7]) {
     // Check that the generated piece is not in a match of 3
     for(int i = 0; i < 5; i++) {
         for(int j = 0; j < 7; j++) {
-            leftPieces = j > 1 && board[i][j].num == board[i][j - 1].num == board[i][j - 2].num;
-            rightPieces = j < 5 && board[i][j].num == board[i][j + 1].num == board[i][j + 2].num;
-            topPieces = i > 1 && board[i][j].num == board[i - 1][j].num == board[i - 2][j].num;
-            bottomPieces = i < 4 && board[i][j].num == board[i + 1][j].num == board[i + 2][j].num;
+            leftPieces = j > 1 && board[i][j].num == board[i][j - 1].num && board[i][j - 1].num == board[i][j - 2].num;
+            rightPieces = j < 5 && board[i][j].num == board[i][j + 1].num && board[i][j + 1].num == board[i][j + 2].num;
+            topPieces = i > 1 && board[i][j].num == board[i - 1][j].num && board[i - 1][j].num == board[i - 2][j].num;
+            bottomPieces = i < 3 && board[i][j].num == board[i + 1][j].num && board[i + 1][j].num == board[i + 2][j].num;
 
             // Check pieces around tile
             while (leftPieces || rightPieces || topPieces || bottomPieces) {
                 board[i][j].num = 1 + (board[i][j].num % 5);
 
-                leftPieces = j > 1 && board[i][j].num == board[i][j - 1].num == board[i][j - 2].num;
-                rightPieces = j < 5 && board[i][j].num == board[i][j + 1].num == board[i][j + 2].num;
-                topPieces = i > 1 && board[i][j].num == board[i - 1][j].num == board[i - 2][j].num;
-                bottomPieces = i < 4 && board[i][j].num == board[i + 1][j].num == board[i + 2][j].num;
+                leftPieces = j > 1 && board[i][j].num == board[i][j - 1].num && board[i][j - 1].num == board[i][j - 2].num;
+                rightPieces = j < 5 && board[i][j].num == board[i][j + 1].num && board[i][j + 1].num == board[i][j + 2].num;
+                topPieces = i > 1 && board[i][j].num == board[i - 1][j].num && board[i - 1][j].num == board[i - 2][j].num;
+                bottomPieces = i < 3 && board[i][j].num == board[i + 1][j].num && board[i + 1][j].num == board[i + 2][j].num;
             }
         }
     }
 }
 
 void pieceShuffle(piece board[5][7]) {
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 7; j++) {
+            if (board[i + 1][j].num == 0 && board[i][j].num != 0) {
+                cout << "YES UNDERNEATH " << to_string(i + 1) << " " << to_string(j + 1) << endl;
+                piece temp = board[i][j];
+                board[i][j] = board[i + 1][j];
+                board[i + 1][j] = temp;
+            }
+        }
+    }
 
+    for (int i = 2; i > -1; i--) {
+        for(int j = 6; j > -1; j--) {
+            if (board[i + 1][j].num == 0 && board[i][j].num != 0) {
+                piece temp = board[i][j];
+                board[i][j] = board[i + 1][j];
+                board[i + 1][j] = temp;
+            }
+        }
+    }
+
+    cout << endl;
+    printBoard(board);
+    cout << endl;
+    generateNewPieces(board);
+    matchChecking(board);
+    printBoard(board);
 }
 
 void getMoves(piece board[5][7]) {
@@ -85,6 +113,11 @@ void getMoves(piece board[5][7]) {
         cout << "What piece do want to move? { " << movesLeft << " moves left } : ";
         cin >> xPosition >> yPosition >> direction;
 
+        if (xPosition == "skip" || yPosition == "skip" || direction == "skip") {
+            movesLeft = 0;
+            continue;
+        }
+
         if(stoi(xPosition) < 1 || stoi(xPosition) > 5) {
             cout << "X coordinate is out of bounds, try another input" << endl;
         }
@@ -93,42 +126,51 @@ void getMoves(piece board[5][7]) {
             cout << "Y coordinate is out of bounds, try another input" << endl;
         }
         
-        else if(direction != "right" && direction != "left" && direction != "up" && direction != "down") {
+        else if(direction != "right" && direction != "left" && direction != "up" && direction != "down" 
+             && direction != "up_left" && direction != "up_right" && direction != "down_left" && direction != "down_right") {
             cout << "Incorrect direction, try another input" << endl;
         }
 
-        else if((stoi(xPosition) == 1 && direction == "up") || (stoi(xPosition) == 5 && direction == "down") ||
-            (stoi(yPosition) == 1 && direction == "left") || (stoi(yPosition) == 7 && direction == "right")) {
+        else if((stoi(xPosition) == 1 && (direction == "up" || direction == "up_left" || direction == "up_right")) || 
+                (stoi(xPosition) == 5 && (direction == "down" || direction == "down_left" || direction == "down_right")) ||
+                (stoi(yPosition) == 1 && (direction == "left" || direction == "up_left" || direction == "down_left")) || 
+                (stoi(yPosition) == 7 && (direction == "right" || direction == "up_right" || direction == "down_right"))) {
             cout << "Direction not possible, try another input" << endl;
         }
 
         else {
-            xCoordinate = stoi(xPosition) + 1;
-            yCoordinate = stoi(yPosition) + 1;
+            xCoordinate = stoi(xPosition) - 1;
+            yCoordinate = stoi(yPosition) - 1;
             if(board[xCoordinate][yCoordinate].matched) {
                 cout << "Piece already matched, try another input" << endl;
                 continue;
             }
 
-            int temp = board[xCoordinate][yCoordinate].num;
+            piece temp = board[xCoordinate][yCoordinate];
             if (direction == "up") {
-                if (board[xCoordinate + 1][yCoordinate].matched) {
-                    cout << "Piece you want to switch is already matched, try another input" << endl;
-                    continue;
-                }
-
-                board[xCoordinate][yCoordinate].num = board[xCoordinate + 1][yCoordinate].num;
-                board[xCoordinate + 1][yCoordinate].num = temp;
-            }
-
-            else if (direction == "down") {
                 if (board[xCoordinate - 1][yCoordinate].matched) {
                     cout << "Piece you want to switch is already matched, try another input" << endl;
                     continue;
                 }
 
-                board[xCoordinate][yCoordinate].num = board[xCoordinate - 1][yCoordinate].num;
-                board[xCoordinate - 1][yCoordinate].num = temp;
+                board[xCoordinate][yCoordinate] = board[xCoordinate - 1][yCoordinate];
+                board[xCoordinate - 1][yCoordinate] = temp;
+
+                checkMatchAfterMove(board, xCoordinate, yCoordinate);
+                checkMatchAfterMove(board, xCoordinate - 1, yCoordinate);
+            }
+
+            else if (direction == "down") {
+                if (board[xCoordinate + 1][yCoordinate].matched) {
+                    cout << "Piece you want to switch is already matched, try another input" << endl;
+                    continue;
+                }
+
+                board[xCoordinate][yCoordinate] = board[xCoordinate + 1][yCoordinate];
+                board[xCoordinate + 1][yCoordinate] = temp;
+
+                checkMatchAfterMove(board, xCoordinate, yCoordinate);
+                checkMatchAfterMove(board, xCoordinate + 1, yCoordinate);
             }
 
             else if (direction == "left") {
@@ -137,8 +179,11 @@ void getMoves(piece board[5][7]) {
                     continue;
                 }
 
-                board[xCoordinate][yCoordinate].num = board[xCoordinate][yCoordinate - 1].num;
-                board[xCoordinate][yCoordinate - 1].num = temp;
+                board[xCoordinate][yCoordinate] = board[xCoordinate][yCoordinate - 1];
+                board[xCoordinate][yCoordinate - 1] = temp;
+
+                checkMatchAfterMove(board, xCoordinate, yCoordinate);
+                checkMatchAfterMove(board, xCoordinate, yCoordinate - 1);
             }
 
             else if (direction == "right") {
@@ -146,9 +191,64 @@ void getMoves(piece board[5][7]) {
                     cout << "Piece you want to switch is already matched, try another input" << endl;
                     continue;
                 }
+                
+                board[xCoordinate][yCoordinate] = board[xCoordinate][yCoordinate + 1];
+                board[xCoordinate][yCoordinate + 1] = temp;
 
-                board[xCoordinate][yCoordinate].num = board[xCoordinate][yCoordinate + 1].num;
-                board[xCoordinate][yCoordinate + 1].num = temp;
+                checkMatchAfterMove(board, xCoordinate, yCoordinate);
+                checkMatchAfterMove(board, xCoordinate, yCoordinate + 1);
+            }
+
+            else if (direction == "up_left") {
+                if (board[xCoordinate - 1][yCoordinate - 1].matched) {
+                    cout << "Piece you want to switch is already matched, try another input" << endl;
+                    continue;
+                }
+
+                board[xCoordinate][yCoordinate] = board[xCoordinate - 1][yCoordinate - 1];
+                board[xCoordinate - 1][yCoordinate - 1] = temp;
+
+                checkMatchAfterMove(board, xCoordinate, yCoordinate);
+                checkMatchAfterMove(board, xCoordinate - 1, yCoordinate - 1);
+            }
+
+            else if (direction == "up_right") {
+                if (board[xCoordinate - 1][yCoordinate + 1].matched) {
+                    cout << "Piece you want to switch is already matched, try another input" << endl;
+                    continue;
+                }
+
+                board[xCoordinate][yCoordinate] = board[xCoordinate - 1][yCoordinate + 1];
+                board[xCoordinate - 1][yCoordinate + 1] = temp;
+
+                checkMatchAfterMove(board, xCoordinate, yCoordinate);
+                checkMatchAfterMove(board, xCoordinate - 1, yCoordinate + 1);
+            }
+
+            else if (direction == "down_left") {
+                if (board[xCoordinate + 1][yCoordinate - 1].matched) {
+                    cout << "Piece you want to switch is already matched, try another input" << endl;
+                    continue;
+                }
+
+                board[xCoordinate][yCoordinate] = board[xCoordinate + 1][yCoordinate - 1];
+                board[xCoordinate + 1][yCoordinate - 1] = temp;
+
+                checkMatchAfterMove(board, xCoordinate, yCoordinate);
+                checkMatchAfterMove(board, xCoordinate + 1, yCoordinate - 1);
+            }
+
+            else if (direction == "down_right") {
+                if (board[xCoordinate + 1][yCoordinate + 1].matched) {
+                    cout << "Piece you want to switch is already matched, try another input" << endl;
+                    continue;
+                }
+
+                board[xCoordinate][yCoordinate] = board[xCoordinate + 1][yCoordinate + 1];
+                board[xCoordinate + 1][yCoordinate + 1] = temp;
+
+                checkMatchAfterMove(board, xCoordinate, yCoordinate);
+                checkMatchAfterMove(board, xCoordinate + 1, yCoordinate + 1);
             }
 
 
@@ -157,4 +257,52 @@ void getMoves(piece board[5][7]) {
 
         printBoard(board);
     } while (movesLeft > 0);
+
+    removeMatched(board);
+    pieceShuffle(board);
+}
+
+void checkMatchAfterMove(piece board[5][7], int x, int y) {
+    // Check left pieces
+    if (y > 1 && board[x][y].num == board[x][y - 1].num && board[x][y - 1].num == board[x][y - 2].num) {
+        board[x][y].matched = board[x][y - 1].matched = board[x][y - 2].matched = true;
+    }
+    // Check right pieces
+    if (y < 5 && board[x][y].num == board[x][y + 1].num && board[x][y + 1].num == board[x][y + 2].num) {
+        board[x][y].matched = board[x][y + 1].matched = board[x][y + 2].matched = true;
+    }
+    // Check top pieces
+    if (x > 1 && board[x][y].num == board[x - 1][y].num && board[x - 1][y].num == board[x - 2][y].num) {
+        board[x][y].matched = board[x - 1][y].matched = board[x - 2][y].matched = true;
+    }
+    // Check bottom pieces
+    if (x < 3 && board[x][y].num == board[x + 1][y].num && board[x + 1][y].num == board[x + 2][y].num) {
+        board[x][y].matched = board[x + 1][y].matched = board[x + 2][y].matched = true;
+    }
+    // Check middle piece vertically
+    if (x > 0 && x < 4 && board[x - 1][y].num == board[x][y].num && board[x][y].num == board[x + 1][y].num) {
+        board[x - 1][y].matched = board[x][y].matched = board[x + 1][y].matched = true;
+    }
+    // Check middle piece horizontally
+    if (y > 0 && y < 6 && board[x][y - 1].num == board[x][y].num && board[x][y].num == board[x][y + 1].num) {
+        board[x][y - 1].matched = board[x][y].matched = board[x][y + 1].matched = true;
+    }
+}
+
+void removeMatched(piece board[5][7]) {
+    string matched = "";
+
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 7; j++) {
+            if (board[i][j].matched) {
+                int x = i;
+                int y = j;
+                matched += to_string(board[i][j].num) + ":[" + to_string(x + 1) + "," + to_string(y + 1) + "]  ";
+                board[i][j].num = 0;
+                board[i][j].matched = false;
+            }
+        }
+    }
+    cout << endl << matched << endl;
+    printBoard(board);
 }
